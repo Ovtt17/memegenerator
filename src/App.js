d0101c25329fe4fa776f1c4da19d1f991324c501
ovtt17 <ovettmora17@gmail.com>
2023-12-17 21:42:17 -0600

    complete meme generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import html2canvas from 'html2canvas';
 
 function App() {
+  const [lines, setLines] = useState({
+    line1: '',
+    line2: '',
+  });
+
+  const [image, setImage] = useState('');
+  
+  const onChangeLine = (line) => (e) => {
+    setLines({
+      ...lines,
+      [line]: e.target.value
+    });
+  };
+
+  const onChangeImage = (e) => {
+    setImage(e.target.value);
+  }
+
+  const exportImage = (e) => {
+    html2canvas(document.querySelector("#meme")).then(canvas => {
+      let img = canvas.toDataURL("image/png");
+
+      let link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+  });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='controllers'>
+      <select onChange={onChangeImage} className='image-selector'>
+        <option value="fire">House Burning</option>
+        <option value= "futurama">Futurama</option>
+        <option value= "history">History Channel</option>
+        <option value= "matrix">Matrix</option>
+        <option value= "philosoraptor">Philosoraptor</option>
+        <option value= "smart">Smart Guy</option>
+      </select> <br/>
+
+      <input onChange={onChangeLine('line1')} type='text' placeholder='Linea 1'/> <br/>
+      <input onChange={onChangeLine('line2')} type='text'placeholder='Linea 2'/> <br/>
+      <button onClick={exportImage}>Export</button>
+      </div>
+
+      <div className='meme' id='meme'>
+        <span>{lines.line1}</span> <br/>
+        <span>{lines.line2}</span> <br/>
+        <img src={"/img/" + image + ".jpg"}/>
+      </div>
     </div>
   );
 }
